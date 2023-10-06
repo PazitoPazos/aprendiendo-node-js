b48318dfeea566fa4e62e8dd2ac075953822131f
PazitoPazos <javier.pazos001@gmail.com>
2023-10-06 22:44:03 +0200

    Added picocolors and .gitignore

--- 8.ls-advanced.js
+++ 8.ls-advanced.js
@@ -1,5 +1,6 @@
 const fs = require('node:fs/promises')
 const path = require('node:path')
+const pc = require('picocolors')
 
 const folder = process.argv[2] ?? '.'
 
@@ -8,7 +9,7 @@ async function ls(folder) {
   try {
     files = await fs.readdir(folder)
   } catch (error) {
-    console.error(`No se pudo leer el directorio ${folder}`)
+    console.error(pc.red(`❌ No se pudo leer el directorio ${folder}`))
     process.exit(1)
   }
 
@@ -28,7 +29,7 @@ async function ls(folder) {
     const fileSize = stats.size.toString()
     const fileModified = stats.mtime.toLocaleString()
 
-    return `${fileType} ${file.padEnd(32)} ${fileSize.padStart(10)} ${fileModified}`
+    return `${pc.white(fileType)} ${pc.blue(file.padEnd(32))} ${pc.green(fileSize.padStart(10))} ${pc.yellow(fileModified)}`
   })
 
   const filesInfo = await Promise.all(filePromises)
